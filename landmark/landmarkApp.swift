29a680dea282e9672be5c2c762aabdaf24cf4428
edolubis21 <edolubis12345@gmail.com>
2022-03-30 19:23:03 +0700

    Initial Commit

---
+++ landmark/landmarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  landmarkApp.swift
+//  landmark
+//
+//  Created by Rekeningku on 30/03/22.
+//
+
+import SwiftUI
+
+@main
+struct landmarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
